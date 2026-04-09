13a1f43e5103a1a92c984c2764b6b8caed01f907
usamaabutt <usamaabutt081@gmail.com>
2026-04-09 23:19:52 +0500

    implemented Apple signIn

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  /** Hosted Lovable app URL (OAuth broker). Used on native so /~oauth resolves correctly. */
+  readonly VITE_LOVABLE_APP_ORIGIN?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
